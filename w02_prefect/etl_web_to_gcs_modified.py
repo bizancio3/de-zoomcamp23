50be35b024ae3a333ca141e288aaee7633d579d6
Javier Villarrubia <bizancio3@gmail.com>
2023-02-01 20:43:16 +0000

    connecting to prefect cloud

--- w02_prefect/etl_web_to_gcs_modified.py
+++ w02_prefect/etl_web_to_gcs_modified.py
@@ -2,14 +2,11 @@ from pathlib import Path
 import pandas as pd
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
-from random import randint
 
 
 @task(retries=3)
 def fetch(dataset_url: str) -> pd.DataFrame:
     """Read taxi data from web into pandas DataFrame"""
-    # if randint(0, 1) > 0:
-    #     raise Exception
 
     df = pd.read_csv(dataset_url)
     return df
