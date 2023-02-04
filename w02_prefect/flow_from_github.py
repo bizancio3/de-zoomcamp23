95f6bc5324512e523893b9ce375e974de8e57492
Javier Villarrubia <bizancio3@gmail.com>
2023-02-04 21:14:57 +0000

    deploying... w02

--- w02_prefect/flow_from_github.py
+++ w02_prefect/flow_from_github.py
@@ -1,19 +1,34 @@
+# Recipe reference from prefect docs:
+# https://towardsdatascience.com/create-robust-data-pipelines-with-prefect-docker-and-github-12b231ca6ed2
+
+""" example running from command line:
+
+prefect deployment build src/main.py:create_pytrends_report \
+  -n google-trends-gh-docker \          #name
+  -q test \                             #queue name
+  -sb github/pytrends \                 #storage block with path to repo 
+  -ib docker-container/google-trends \  #docker block (deployed artifact)
+  -o prefect-docker-deployment \        #outputs .yaml file
+  --apply
+
+"""
+
+# or embedded in python script
+
 from prefect.filesystems import GitHub
 
-github = GitHub(
-    access_token="YOUR_GITHUB_ACCESS_TOKEN", 
-    repo="owner/repo"
-    )
+github = GitHub(access_token="YOUR_GITHUB_ACCESS_TOKEN", repo="owner/repo")
 
 flow_file = "path/to/flow.py"
 flow_content = github.read(flow_file)
 
 
-# To get a GitHub access token, 
-# you need to have a GitHub account and follow these steps:
+""" 
+Steps for getting a GitHub access token:
 
-# - Go to the GitHub Developer Settings page (https://github.com/settings/developers)
-# - Click on the "Personal access tokens" section.
-# - Click the "Generate new token" button.
-# - Provide a descriptive name for your token, 
-#   select the scope of access you need, and click the "Generate token" button
+- Go to the GitHub Developer Settings page (https://github.com/settings/developers)
+- Click on the "Personal access tokens" section.
+- Click the "Generate new token" button.
+- Provide a descriptive name for your token, 
+select the scope of access you need, and click the "Generate token" button
+"""
